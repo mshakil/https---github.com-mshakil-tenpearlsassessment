2a514dd659a483a73ac71c31107b17861308ab14
Mubbashir_Shakil <mshakil74@gmail.com>
2022-08-26 10:37:41 +0500

    New Changes

--- cypress/page-objects/pages/productdashboard.js
+++ cypress/page-objects/pages/productdashboard.js
@@ -4,4 +4,5 @@ export default class ProductDashboard extends BasePage{
     static VerifyAccountCreatedSuccessfully(userName){
         cy.get('h1.welcome-message-text').should('contain.text',userName);
     }
+    
 }
